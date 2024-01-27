bf8cede4cd46eefe4afbe31622f701dbd2862378
TeotheToad <157289249+TeotheToad@users.noreply.github.com>
2024-01-27 15:14:50 +0330

    Add files via upload

---
+++ Random float from a to b.py
@@ -0,0 +1,15 @@
+class LCG:
+    def __init__(self, seed, x=16467, y=201467, z=2**10):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.state = seed
+
+    def random_number(self):
+        self.state = ((self.y + self.x * self.state) % self.z)
+        return (self.state / int(self.z))
+
+def random_number_a_to_b(a:int, b:int):
+    value = abs(hash(str(a + b)))
+    lcg = LCG(seed=value)
+    return (lcg.random_number()*(b - a) + a)
