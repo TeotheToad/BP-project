619a8cebd0f5f9cd4daff08a05a9e246e056d33c
TeotheToad <157289249+TeotheToad@users.noreply.github.com>
2024-01-27 16:22:35 +0330

    Add files via upload

--- Random float from a to b.py
+++ Random float from a to b.py
@@ -1,15 +1,20 @@
 class LCG:
     def __init__(self, seed, x=16467, y=201467, z=2**10):
-        self.x = x
-        self.y = y
-        self.z = z
-        self.state = seed
+        # مقداردهی اولیه به مولفه‌ها
+        self.x = x  # ضرب‌کننده
+        self.y = y  # افزایش‌دهنده
+        self.z = z  # ماژولوس
+        self.state = seed  # مقدار اولیه
 
     def random_number(self):
+        # تولید یک عدد تصادفی با استفاده از فرمول
         self.state = ((self.y + self.x * self.state) % self.z)
-        return (self.state / int(self.z))
+        return (self.state / int(self.z))  # نرمال‌سازی نتیجه به منظور قرار گرفتن در بازه 0 تا 1
 
 def random_number_a_to_b(a:int, b:int):
+    # تولید یک مقدار ابتدایی بر اساس جمع a و b
     value = abs(hash(str(a + b)))
+    # ایجاد یک نمونه با استفاده از مقدار ابتدایی تولید شده
     lcg = LCG(seed=value)
-    return (lcg.random_number()*(b - a) + a)
+    # تولید یک عدد رندوم در بازه [a، b)
+    return (lcg.random_number() * (b - a) + a)  # تولید عدد رندوم در بازه (a تا b)
